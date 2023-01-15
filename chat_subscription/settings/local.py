a1d8dd508873107417643833bbf45a6bd11e610a
Alteian <davosroucka@gmail.com>
2023-01-15 17:42:49 +0100

    Project init

---
+++ chat_subscription/settings/local.py
@@ -0,0 +1,26 @@
+from .base import *
+
+# MIDDLEWARE += ["silk.middleware.SilkyMiddleware"]
+
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {"console": {"class": "logging.StreamHandler"}},
+    "loggers": {
+        "django.request": {
+            "handlers": ["console"],
+            "level": "DEBUG",
+            "propagate": False,
+        },
+        "qinspect": {
+            "handlers": ["console"],
+            "level": "DEBUG",
+            "propagate": True,
+        },
+    },
+}
+
+
+MEDIA_ROOT = "/tmp"
+MEDIA_URL = "/files/"
